c95e794872c7d1c63ce2d903e5c65961d2b7650d
Denise Middleton <demiddleton@bellsouth.net>
2019-08-21 18:32:46 -0400

    pseudocode next steps on the js file

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,19 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-//Declare variable to hold array of topics
-var topics = ["Bristletails", "Antlion", "Zygentoma", "Caddisflies", "Dobsonflies", "Scale Insect", "Net-winged Insects", "Booklice", "Orthoptera", "Pteryota", "Odonata", "Caelifera"];
+    //Declare variable to hold array of topics
+    var topics = ["Bristletails", "Antlion", "Zygentoma", "Caddisflies", "Dobsonflies", "Scale Insect", "Net-winged Insects", "Booklice", "Orthoptera", "Pteryota", "Odonata", "Caelifera"];
 
-//Create function to display the array of buttons on the page
-function createBtns() {
-    $("#button-area").empty();
+    //Create function to display the array of buttons on the page
+    function createBtns() {
 
-for (var i = 0; i < topics.length; i++) { 
-    var buttons = $("<button>").text(topics[i]);
-    buttons.attr("data-name", topics[i])
-    $("#button-area").append(buttons);
-    console.log(buttons);
-}
+        for (var i = 0; i < topics.length; i++) {
+            var buttons = $("<button>").text(topics[i]);
+            buttons.attr("data-name", topics[i])
+            $("#button-area").append(buttons);
+            console.log(buttons);
+        }
+    
+    }
+    createBtns();
 
-}
-createBtns();
+    //Create a button click function to display 10 non-animated gifs and place them on the page
+
+    //Create click function to animate the gif when the image is clicked
+    
+
 });
